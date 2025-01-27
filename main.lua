2e74d1881003dc3b27a7b0a67c9794a0535593bc
hinyb <40139991+hinyb@users.noreply.github.com>
2025-01-27 20:18:50 +0800

    Add files via upload

--- main.lua
+++ main.lua
@@ -24,6 +24,26 @@ end
 local DRONEOFFSET = 48
 
 local function init()
+    local get_random = Utils.random_skill_id()
+    local drone_skill_blacklist = {
+        [129] = true, -- drifterX -- It's difficult to form a combo. So I decided blacklist it.
+        [130] = true, -- drifterC
+        [131] = true, -- drifterV
+        [132] = true, -- drifterVBoosted
+        [133] = true, -- drifterX2
+        [134] = true, -- drifterC2
+        [135] = true, -- drifterV2
+        [136] = true, -- drifterV2Boosted
+        [141] = true, -- robomandoV
+        [142] = true -- robomandoVBoosted
+    }
+    local function get_drone_random_skill_id()
+        local random_skill_id = get_random()
+        while not Utils.is_damage_skill(random_skill_id) or drone_skill_blacklist[random_skill_id] do
+            random_skill_id = get_random()
+        end
+        return random_skill_id
+    end
     local function get_x_range_min(inst)
         local x_range_min = math.huge
         for slot_index = 0, 3 do
@@ -50,21 +70,30 @@ local function init()
         self:drone_stats_init(200)
         self:init_actor_late()
         CompatibilityPatch.set_compat(self.value)
-        gm.actor_skill_set(self.value, 0, 2)
-        gm.actor_skill_set(self.value, 1, 1)
-        gm.actor_skill_set(self.value, 2, 4)
-        gm.actor_skill_set(self.value, 3, 4)
+        --gm.actor_skill_set(self.value, Utils.get_random(0, 3), get_drone_random_skill_id())
+        gm.actor_skill_set(self.value, Utils.get_random(0, 3), 92)
         self.x_range = 1200
         self.x_range_min = get_x_range_min(self.value)
         self.y_offset = 0
         self.image_alpha = 0.5
 
+        -- to fix combo attack loaderZ and pilotZ
+        self.z_tap_buffered = 1
+        
         -- I just find all skills which use hold_facing_direction_xscale have this bug.
         -- Need time to find a better solution.
         Instance_ext.add_callback(self.value, "pre_skill_activate", "hold_facing_direction_xscale_fix",
             function(inst, slot_index)
                 inst.hold_facing_direction_xscale = inst.image_xscale
             end)
+        Instance_ext.add_callback(self.value, "pre_actor_set_dead", "drop_skill_pickup", function(inst_)
+            for slot_index = 0, 3 do
+                local skill = gm.array_get(inst_.skills, slot_index).active_skill
+                if skill.skill_id ~= 0 then
+                    SkillPickup.drop_skill(inst_, skill)
+                end
+            end
+        end)
     end)
     oSkillDrone:onStep(function(self)
         local master = self.master
@@ -108,7 +137,8 @@ local function init()
         end]]
         local image_index = self.image_index
         local image_number = self.image_number
-        if math.abs(image_index - image_number + 1) <= 0.0001 or image_index >= image_number - 1 then
+        --log.info(image_index, image_number, math.abs(image_index - image_number + 1), math.abs(image_index - image_number + 1) <= 0.0001)
+        if self.state_strafe_half == 1.0 or math.abs(image_index - image_number + 1) <= 0.0001 or image_index >= image_number - 1 then
             self.sprite_index = oSkillDrone.obj_sprite
         end
         self.z_skill = 0
@@ -116,6 +146,7 @@ local function init()
         self.c_skill = 0
         self.v_skill = 0
         if self.state == 0 then
+            --self.sprite_index = oSkillDrone.obj_sprite
             local lerp_factor = (1 - self.chase_motion_lerp) * 0.1111111111111111
             self.x = Utils.lerp(self.x, master.ghost_x + self.xx, lerp_factor)
             self.y = Utils.lerp(self.y, master.ghost_y + self.yy - 100 - DRONEOFFSET, lerp_factor) + self.yo
@@ -130,6 +161,30 @@ local function init()
                     self.target = new_target
                 end
             end]]
+            local skill_pickup = gm._mod_instance_nearest(SkillPickup.skillPickup_object_index, self.x, self.y)
+            if skill_pickup ~= -4 then
+                if math.abs(self.x - skill_pickup.x) <= self.y_range and math.abs(self.y - skill_pickup.y) <=
+                    self.y_range then
+                    local slot_index = skill_pickup.slot_index
+                    local skill_id = skill_pickup.skill_id
+                    gm.call("gml_Script_interactable_set_active", skill_pickup, self.value, skill_pickup, self.value, 1)
+                    local x_range = Utils.skill_get_range(skill_id)
+                    if self.x_range_min > x_range then
+                        self.x_range_min = x_range
+                    end
+                    return
+                end
+                if skill_pickup.fake_target == nil then
+                    skill_pickup.fake_target = gm.instance_create(skill_pickup.x, skill_pickup.y + 80,
+                        FindTargetOverride.empty_object_index)
+                    skill_pickup.fake_target.parent = gm.struct_create()
+                    skill_pickup.fake_target.parent.bbox_bottom = skill_pickup.y
+                    Instance_ext.add_callback(skill_pickup, "pre_destroy", "destroy_fake_target",
+                        function(skill_pickup_)
+                            gm.instance_destroy(skill_pickup_.fake_target.id)
+                        end)
+                end
+            end
         elseif self.state == 1 then
             local target = self.target
             if Instance.exists(target) then
@@ -176,16 +231,23 @@ local function init()
             return
         end
         local slot_index = args[2].value
-        local skill = inst_wrapped:get_active_skill(slot_index)
-        local angular_speed = 1 / math.log(skill.cooldown) * 0.05
+        local skill_id = args[3].value
+        local name = "star_draw" .. Utils.to_string_with_floor(slot_index)
+        if skill_id == 0 then
+            inst_wrapped:remove_callback(name)
+            return
+        end
+        
         local angle = slot_index * math.pi / 2
         local radius = 24
-        local name = "star_draw" .. Utils.to_string_with_floor(slot_index)
-        local default_skill = Class.SKILL:get(skill.skill_id)
+        local default_skill = Class.SKILL:get(skill_id)
         local sprite_index = default_skill:get(4)
         local image_index = default_skill:get(5)
+        local cooldown = default_skill:get(6)
+        local angular_speed = 1 / math.log(cooldown) * 0.05
         local required_stock = default_skill:get(11)
         inst_wrapped:remove_callback(name)
+        local skill = inst_wrapped:get_active_skill(slot_index)
         inst_wrapped:add_callback("onPostDraw", name, function(actor)
             angle = angle + angular_speed
             local x = actor.x + radius * math.cos(angle)
@@ -193,7 +255,8 @@ local function init()
             if skill.stock < required_stock then
                 gm.draw_sprite_ext(sprite_index, image_index, x + 5, y + 5, skill_scale, skill_scale, 0.0, Color.GRAY, 1)
             else
-                gm.draw_sprite_ext(sprite_index, image_index, x + 5, y + 5, skill_scale, skill_scale, 0.0, Color.WHITE, 1)
+                gm.draw_sprite_ext(sprite_index, image_index, x + 5, y + 5, skill_scale, skill_scale, 0.0, Color.WHITE,
+                    1)
             end
         end)
     end)
